2b66b9e548219baf4fcd6ce59a47fe4e926f2eb9
hikvision pc <929244872@qq.com>
2021-11-05 20:27:07 +0800

    部分索引最小优先队列代码

---
+++ inc/index_min_pri_qun.h
@@ -0,0 +1,12 @@
+#ifndef __INDEX_MIN_PRI_QUN_H
+#define __INDEX_MIN_PRI_QUN_H
+
+typedef struct index_min_pri_qun
+{	
+	int *qun_p;
+	int *pq;
+	int *qp;
+	unsigned int num;
+}index_min_qun;
+
+#endif
---
+++ src/index_min_pri_qun.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <malloc.h> 
+#include "index_min_pri_qun.h"
+
+
+static void index_qun_exch(index_min_qun *i_q, int i, int j)
+{
+	int tmp;
+	tmp = i_q->pq[i];
+	i_q->pq[i] = i_q->pq[j];
+	i_q->pq[j] = tmp;
+
+	i_q->qp[i_q->pq[i]] = i;
+	i_q->qp[i_q->pq[j]] = j;
+}
+
+static inline void index_qun_less(index_min_qun *i_q, int i, int j)
+{
+	return i_q->qun_p[i_q->pq[i]] < i_q->qun_p[i_q->pq[j]]? 1 : 0;
+}
+
+
+
+main()
+{
+	index_min_qun i_qun;
+	int qun[20] = { 0 };
+	int pq[20] = { -1 };
+	int qp[20] = { -1 };
+
+	i_qun.num = 0;
+	i_qun.qun_p = qun;
+	i_qun.pq = pq;
+	i_qun.qp = qp;
+
+	
+
+	
+}
+
+
+
