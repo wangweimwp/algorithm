8e7c0fd6980f5ce828403701a145f0b0d32f917e
hikvision pc <929244872@qq.com>
2022-01-21 15:40:35 +0800

    上传并查集代码3

--- inc/union_find.h
+++ inc/union_find.h
@@ -6,7 +6,17 @@ typedef struct union_find_struct{
 	int num;
 }UF;
 
+
+typedef struct union_find_tree_weight_struct{
+	int *ele_and_group;
+	int *size;
+	int num;
+}UF_tree_weight;
+
+
 void union_find(void);
+void uf_tw_test(void);
+
 
 #endif
 
--- src/union_find.c
+++ src/union_find.c
@@ -63,3 +63,82 @@ void union_find(void)
 	printf("%d\n", uf_group.num);
 }
 
+/*************************************************************************************************/
+
+static void uf_tw_init(UF_tree_weight *uf_tw_point, int n)
+{
+	int i;
+	if(uf_tw_point == NULL)
+		return;
+	
+	uf_tw_point->num = n;
+	uf_tw_point->ele_and_group =(int *) malloc(sizeof(int) * n);
+	uf_tw_point->size =(int *) malloc(sizeof(int) * n);
+
+	for(i = 0; i < n; i++){
+		uf_tw_point->ele_and_group[i] = i;
+		uf_tw_point->size[i] = 1;
+	}
+
+	return ;
+}
+
+static int uf_tw_find(UF_tree_weight *uf_tw_point, int p)
+{
+	int tmp_p = p;
+	while(tmp_p != uf_tw_point->ele_and_group[tmp_p]){
+		tmp_p = uf_tw_point->ele_and_group[tmp_p];
+	}
+		
+	return tmp_p;
+}
+
+static int uf_tw_is_connected(UF_tree_weight *uf_tw_point, int p, int q)
+{
+	return uf_tw_find(uf_tw_point, p) == uf_tw_find(uf_tw_point, q);
+	
+}
+
+static void uf_tw_union_group(UF_tree_weight *uf_tw_point, int p, int q)
+{
+	int p_root, q_root;
+	int i;
+	if(uf_tw_is_connected(uf_tw_point, p, q)){
+		return ;
+	}
+
+	p_root = uf_tw_find(uf_tw_point, p);
+	q_root = uf_tw_find(uf_tw_point, q);
+
+	if(uf_tw_point->size[p_root] > uf_tw_point->size[q_root]){
+		uf_tw_point->ele_and_group[q_root] = p_root;
+		uf_tw_point->size[p_root] += uf_tw_point->size[q_root];
+	}else{
+		uf_tw_point->ele_and_group[p_root] = q_root;
+		uf_tw_point->size[q_root] += uf_tw_point->size[p_root];
+	}
+	uf_tw_point->num --;
+	return;
+}
+
+void uf_tw_test(void)
+{
+	UF_tree_weight uf_tw_group;
+
+	uf_tw_init(&uf_tw_group, 10);
+	uf_tw_union_group(&uf_tw_group, 1, 2);
+	printf("%d\n", uf_tw_group.num);
+	printf("%d, %d\n", uf_tw_is_connected(&uf_tw_group, 1, 2),
+						uf_tw_is_connected(&uf_tw_group, 1, 3));
+	uf_tw_union_group(&uf_tw_group, 1, 2);
+	printf("%d\n", uf_tw_group.num);
+	printf("%d, %d\n", uf_tw_is_connected(&uf_tw_group, 1, 2), 
+						uf_tw_is_connected(&uf_tw_group, 1, 3));
+	uf_tw_union_group(&uf_tw_group, 2, 3);
+	printf("%d\n", uf_tw_group.num);
+	printf("%d, %d\n", uf_tw_is_connected(&uf_tw_group, 1, 2), 
+						uf_tw_is_connected(&uf_tw_group, 1, 3));
+}
+
+
+
