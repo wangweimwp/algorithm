13668fef9bd9936bd1bd913f50a913ed2b6a821e
hikvision pc <929244872@qq.com>
2022-01-25 20:28:26 +0800

    图的广度优先搜索1

--- inc/graph.h
+++ inc/graph.h
@@ -6,11 +6,20 @@ typedef struct dfs_struct{
 	int *marked;
 	int searched;
 };
+
+typedef struct bfs_struct{
+	int *marked;
+	int *queue_tmp;
+	int q_p1, q_p2;
+	int searched;
+};
+
 typedef struct graph_struct{
 	int point;
 	int edge;
 	int queue[100][100];
 	struct dfs_struct dfs;
+	struct bfs_struct bfs;
 }graph;
 
 
--- src/graph.c
+++ src/graph.c
@@ -10,14 +10,20 @@ static void graph_init(graph *graph_p, int point)
 	graph_p->edge = 0;
 	//graph_p->queue = (int **)malloc(point * point * sizeof(int));
 	graph_p->dfs.marked = (int *)malloc(point * sizeof(int));
+	graph_p->bfs.marked = (int *)malloc(point * sizeof(int));
+	graph_p->bfs.queue_tmp = (int *)malloc(point * sizeof(int));
 	for(i = 0; i < point; i++){
 		for(j = 0; j < point; j++){
 			graph_p->queue[i][j] = -1;
 		}
 		graph_p->dfs.marked[i] = -1;
+		graph_p->bfs.marked[i] = -1;
+		graph_p->bfs.queue_tmp[i] = -1;
 	}
 	graph_p->dfs.searched = 0;
-	
+	graph_p->bfs.searched = 0;
+	graph_p->bfs.q_p1 = 0;
+	graph_p->bfs.q_p2 = 0;
 	return;
 }
 
@@ -56,6 +62,28 @@ static void dfs(graph *graph_p, int point)
 	return;
 }
 
+static void bfs(graph *graph_p, int point)
+{
+/*感觉有问题，和深度优先搜索逻辑一样*/
+	int tmp = 0;
+	int i;
+	graph_p->bfs.marked[point] = 1;
+	graph_p->bfs.queue_tmp[graph_p->bfs.q_p1] = point;
+	graph_p->bfs.q_p1 ++;
+
+	while(graph_p->bfs.queue_tmp[graph_p->bfs.q_p2] != -1){
+		tmp = graph_p->bfs.queue_tmp[graph_p->bfs.q_p2];
+		graph_p->bfs.q_p2 ++;
+		for(i = 0; graph_p->queue[tmp][i] != -1; i++){
+			if(graph_p->bfs.marked[graph_p->queue[tmp][i]] == -1){
+				
+				bfs(graph_p, graph_p->queue[tmp][i]);
+			}
+		}
+	}
+	graph_p->bfs.searched++;
+}
+
 static int is_connected(graph *graph_p, int point1, int point2)
 {
 	dfs(graph_p, point1);
@@ -91,10 +119,13 @@ void graph_test(void)
 	
 	print_queue(&gra, 6);
 
-	ret = edge_count(&gra, 7);
+	ret = edge_count(&gra, 0);
 	printf("ret = %d\n", ret);
 	ret = is_connected(&gra, 5, 6);
 	printf("ret = %d\n", ret);
+
+	bfs(&gra, 0);
+	printf("searched = %d\n", gra.bfs.searched);
 }
 
 
